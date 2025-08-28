2a8b9ad6cb86420b129cd96c048b633ec0a0a80c
Lyl094 <elaine0silva19@gmail.com>
2025-08-28 14:40:32 -0300

    Add files via upload

---
+++ Comparação.pas
@@ -0,0 +1,13 @@
+Program comparacao;
+Uses CRT;
+Var x,y: Integer;
+
+Begin 
+	Write('Entre com 2 números:');
+	Read (X,Y);
+	If (X>Y) Then
+		Write ('X é Maior que Y, e seu valor é=',X)
+	Else
+		Write('Y é Maior que X, e seu valor é=',Y);
+Readkey; 
+End.                                                  
